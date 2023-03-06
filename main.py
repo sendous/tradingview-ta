8a9d14a9e680b4d097c9a0952b2374f3657c7ec0
mehdi <sendous69@gmail.com>
2023-03-06 19:33:53 +0330

    add other intervals

--- main.py
+++ main.py
@@ -11,9 +11,8 @@ async def interval_day(symbol):
             interval=ta.Interval.INTERVAL_1_DAY
         )
         analysis_day = handler_day.get_analysis().summary
-        # if analysis_day['RECOMMENDATION'] == 'BUY':
-        print(f'Day: {analysis_day} : {symbol}')
-        print('\n')
+        if analysis_day['RECOMMENDATION'] == 'BUY':
+            print(f'Day: {analysis_day} : {symbol}')
     except:
         pass
 
@@ -26,9 +25,50 @@ async def interval_h4(symbol):
             interval=ta.Interval.INTERVAL_4_HOURS
         )
         analysis_4H = handler_4H.get_analysis().summary
-        # if analysis_4H['RECOMMENDATION'] == 'BUY':
-        print(f'H4: {analysis_4H} : {symbol}')
-        print('\n')
+        if analysis_4H['RECOMMENDATION'] == 'BUY':
+            print(f'H4: {analysis_4H} : {symbol}')
+    except:
+        pass
+
+async def interval_h1(symbol):
+    try:
+        handler_1H = ta.TA_Handler(
+            symbol=symbol,
+            screener='crypto',
+            exchange='BINANCE',
+            interval=ta.Interval.INTERVAL_1_HOURS
+        )
+        analysis_1H = handler_1H.get_analysis().summary
+        if analysis_1H['RECOMMENDATION'] == 'BUY':
+            print(f'H1: {analysis_1H} : {symbol}')
+    except:
+        pass
+
+async def interval_15min(symbol):
+    try:
+        handler_15min = ta.TA_Handler(
+            symbol=symbol,
+            screener='crypto',
+            exchange='BINANCE',
+            interval=ta.Interval.INTERVAL_15_MINUTES
+        )
+        analysis_15min = handler_15min.get_analysis().summary
+        if analysis_15min['RECOMMENDATION'] == 'BUY':
+            print(f'15Min: {analysis_15min} : {symbol}')
+    except:
+        pass
+
+async def interval_1min(symbol):
+    try:
+        handler_1min = ta.TA_Handler(
+            symbol=symbol,
+            screener='crypto',
+            exchange='BINANCE',
+            interval=ta.Interval.INTERVAL_1_MINUTES
+        )
+        analysis_1min = handler_1min.get_analysis().summary
+        if analysis_1min['RECOMMENDATION'] == 'BUY':
+            print(f'1Min: {analysis_1min} : {symbol}')
     except:
         pass
 
@@ -37,8 +77,9 @@ async def main():
         reader = csv.reader(file)
         for row in reader:
             symbol = row[0].strip()
-            await interval_day(symbol)
-            await interval_h4(symbol)
+            await interval_day(symbol), interval_h4(symbol),
+            interval_h1(symbol), interval_15min(symbol), interval_1min(symbol)
+            print('\n')
 
 asyncio.run(main())
 
